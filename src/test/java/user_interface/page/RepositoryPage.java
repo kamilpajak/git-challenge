705181ebadc7d34c51d63bf0a75cd5ea3a367ee9
Kamil Pajak <kamil.pajak@performgroup.com>
2018-03-19 00:04:38 +0100

    Improved RepositoryPage.createNewBranch() method.

--- src/test/java/user_interface/page/RepositoryPage.java
+++ src/test/java/user_interface/page/RepositoryPage.java
@@ -10,6 +10,11 @@ public class RepositoryPage extends RepositoryBasePage {
         $(".branch-select-menu").click();
         $("#context-commitish-filter-field").should(Condition.appear).setValue(branch);
         $(".js-new-item-name").should(Condition.appear).click();
+        $(".branch-select-menu .js-menu-target").shouldHave(Condition.attribute("title", branch));
         return this;
+    }
+
+    public void createNewFile(String filename) {
+        $(".file-navigation button[type=submit]").click();
     }
 }
