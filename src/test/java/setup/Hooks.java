9c3a6359ebeea2348f1011496af320ea48e85d95
Kamil Pajak <kamil.pajak@performgroup.com>
2018-11-30 00:40:36 +0100

    Move setup() into @Before hook

--- src/test/java/setup/Hooks.java
+++ src/test/java/setup/Hooks.java
@@ -6,14 +6,23 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static cucumber.api.Result.Type.PASSED;
+import static setup.Settings.properties;
 
 public class Hooks {
 
     @Before
     public void before(Scenario scenario) {
         Settings.scenario = scenario.getName();
+        Settings.setUp();
+        Environment environment = Environment.valueOf(properties.getProperty("github.environment").toUpperCase());
+        switch (environment) {
+            case PRODUCTION:
+                open(Environment.PRODUCTION.url());
+                break;
+        }
     }
 
     @After
